c27e18f321099114a9992f7c8e42a60dfca5c29c
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2023-09-03 04:04:42 -0600

    primer commit

--- src/src/Servidor.java
+++ src/src/Servidor.java
@@ -7,11 +7,5 @@ public class Servidor {
         System.out.print("Hello and welcome!");
 
         // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
