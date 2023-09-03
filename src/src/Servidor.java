81a7ff54575f93b5af313ae70646e6cfed7a474c
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2023-09-03 04:52:22 -0600

    Borrador de creacion de lista

--- src/src/Servidor.java
+++ src/src/Servidor.java
@@ -1,11 +1,58 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+class Node {
+    private Object data;
+    protected Node next;
+
+    public Node(Object data){
+        this.next = null;
+        this.data = data;
+    }
+    public Object getData(){
+        return this.data;
+    }
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return this.next;
+    }
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
+class LinkedList{
+    private Node head;
+    private int size;
+    private Node tail;
+
+    public LinkedList(){
+        this.head=null;
+        this.size=0;
+        this.tail=null;
+    }
+    public int size() {
+        return this.size;
+    }
+    public void insert(Object data){
+        if(head==null){
+            head=tail= new Node(data);
+        }else{
+            tail=tail.next = new Node(data);
+        }
+    }
+
+}
 public class Servidor {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        Object lista1 = new LinkedList();
+        Object lista2 = new LinkedList();
+        Object lista3 = new LinkedList();
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
+        lista1.insert(8);
+        lista2.insert(5);
+        lista3.insert(4);
+
+
     }
 }
