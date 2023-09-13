9f9887196213e92ca26149e288e35c5587ea28f3
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2023-09-12 22:35:26 -0600

    codigo mas bonito y ordenado

--- src/src/Cliente.java
+++ src/src/Cliente.java
@@ -7,48 +7,62 @@ import javax.swing.*;
 import java.awt.*;
 
 class VentanaClient extends JFrame{
+    private JPanel panel;
+    private JLabel etiqueta;
+    private JTextField chatTexto;
+    private JButton boton;
+
+
     public VentanaClient(){
         this.setBounds(500,200,400,400);
         setTitle("Cliente");
+
         Componentes();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
     private void Componentes(){
-        JPanel panel = new JPanel();
+        colocarPanel();
+        colocarEtiqueta();
+        colocarCajadeTexto();
+        colocarBoton();
+    }
+    private void colocarPanel(){
+        panel = new JPanel();
+        panel.setLayout(null);
         this.getContentPane().add(panel);
-        //panel.setBackground(new Color(118,181,197));
-
-        JLabel etiqueta = new JLabel("Cliente",SwingConstants.CENTER);
+    }
+    private void colocarEtiqueta(){
+        etiqueta = new JLabel("Cliente",SwingConstants.CENTER);
         panel.add(etiqueta);
         etiqueta.setBounds(140,10,100,25);
         etiqueta.setForeground(Color.WHITE);
         etiqueta.setBackground(Color.BLACK);
         etiqueta.setFont(new Font("times new roman", Font.PLAIN,20));
-
-        JTextField chatTexto = new JTextField();
+        etiqueta.setOpaque(true);
+    }
+    private void colocarCajadeTexto(){
+        chatTexto = new JTextField();
         chatTexto.setBounds(60,50,250,20);
         panel.add(chatTexto);
-
-        JButton boton = new JButton("Enviar");
+    }
+    private void colocarBoton(){
+        boton = new JButton("Enviar");
         boton.setBounds(140,80,100,30);
         panel.add(boton);
-
-        etiqueta.setOpaque(true);
-        panel.setLayout(null);
         boton.setEnabled(true);
 
         ActionListener enviaTexto = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                System.out.println(chatTexto.getText());;
+                System.out.println(chatTexto.getText());
             }
         };
         boton.addActionListener(enviaTexto);
     }
-
 }
+
 public class Cliente {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         /**instancia de la ventana de servidor
          */
         VentanaClient v1 = new VentanaClient();
